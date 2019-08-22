c7f9189fed998c317648164230452f7dd4094e09
Mark Rego <rego121@gmail.com>
2019-08-22 19:15:55 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+
+  def name(name)
+
+    @name = name
+
+
+  end
+
+
+  def name=(name)
+
+    @name = name
+
+  def breed(breed)
+
+    @breed = breed
+
+  end
+
+
+  def breed=(breed)
+
+end
+
+    
